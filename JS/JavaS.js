a9ac4639b3a851dcec576b0dfe9ca34c7583ac4b
RyricoD321221 <145660524+RyricoD321221@users.noreply.github.com>
2023-11-10 14:27:22 +0300

    Task

---
+++ JS/JavaS.js
@@ -0,0 +1,14 @@
+
+
+let arr = [1,2,34,352];
+function isMonotone(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i+1] = 0) {
+            return true;
+        }
+        else {
+              return false;
+}
+        }
+    }
+ console.log(isMonotone(arr));
